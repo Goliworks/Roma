4f07ed44305556c05c1fb06db55a37ec50e38203
Guillaume (Goliver) Golay <goliworks@gmail.com>
2020-07-09 23:50:00 +0200

    feat: test server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+
+async fn handler() -> String {
+    let hello:String = String::from("Hello, world");
+    hello
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    println!("Run server.");
+    HttpServer::new(|| {
+        App::new()
+            .default_service(web::route().to(handler))
+    })
+        .bind("0:8000")?
+        .run()
+        .await
 }
