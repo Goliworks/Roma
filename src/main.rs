331fc7ec0ce82c55939defe26e021d9a97332dca
Guillaume (Goliver) Golay <goliworks@gmail.com>
2020-07-12 21:02:35 +0200

    feat: simple client

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, Error, HttpServer, HttpRequest, HttpResponse};
+use actix_web::client::Client;
 
-async fn handler() -> String {
-    let hello:String = String::from("Hello, world");
-    hello
+async fn handler(
+    req: HttpRequest,
+    body: web::Bytes,
+    client: web::Data<Client>)
+    -> Result<HttpResponse, Error> {
+    let forwarded_req = client
+        .request_from("http://localhost:3000", req.head())
+        .no_decompress();
+
+    let forwarded_req = if let Some(addr) = req.head().peer_addr {
+        forwarded_req.header("x-forwarded-for", format!("{}", addr.ip()))
+    } else {
+        forwarded_req
+    };
+
+    let res = forwarded_req.send_body(body).await.map_err(Error::from)?;
+
+    let mut client_resp = HttpResponse::build(res.status());
+
+    Ok(client_resp.streaming(res))
 }
 
 #[actix_rt::main]
@@ -10,9 +28,10 @@ async fn main() -> std::io::Result<()> {
     println!("Run server.");
     HttpServer::new(|| {
         App::new()
+            .data(Client::new())
             .default_service(web::route().to(handler))
     })
-        .bind("0:8000")?
+        .bind("0:8080")?
         .run()
         .await
 }
